eafd463aab2d03efd1fe605b01778a1296b33e49
Kris Rivera <kkrivera@gmail.com>
2021-12-04 15:11:46 -0500

    use more efficient data structures for day04

--- 2021/day04.ts
+++ 2021/day04.ts
@@ -1,9 +1,16 @@
 import { readLines, run } from '../run';
 
-const marker = 'X';
-type InputType = string;
-type Board = InputType[][];
-type Input = [InputType[], Board[]];
+// const marker = 'X';
+type Value = string;
+type Board = Value[][];
+type BoardMarks = { [value: Value]: boolean };
+type BoardProps = {
+  board: Board;
+  boardMarks: BoardMarks;
+  totalMarks: number;
+};
+type CalledNumbers = Value[];
+type Input = [CalledNumbers, BoardProps[]];
 
 const part1 = ([calledNumbers, boards]: Input) => {
   for (let i = 0; i < calledNumbers.length; i++) {
@@ -11,7 +18,7 @@ const part1 = ([calledNumbers, boards]: Input) => {
     for (let j = 0; j < boards.length; j++) {
       const board = boards[j];
       markBoard(board, calledNumber);
-      if (i >= 4 && isBoardWinner(board)) {
+      if (isBoardWinner(board)) {
         return calculateScore(board, calledNumber);
       }
     }
@@ -24,7 +31,7 @@ const part2 = ([calledNumbers, boards]: Input) => {
     for (let j = 0; j < boards.length; j++) {
       const board = boards[j];
       markBoard(board, calledNumber);
-      if (i >= 4 && isBoardWinner(board)) {
+      if (isBoardWinner(board)) {
         if (boards.length === 1) {
           return calculateScore(board, calledNumber);
         }
@@ -36,32 +43,34 @@ const part2 = ([calledNumbers, boards]: Input) => {
   }
 };
 
-const calculateScore = (board: Board, lastCalledNumber: InputType) => {
+const calculateScore = ({ board, boardMarks }: BoardProps, lastCalledNumber: Value) => {
   const unmarkedSum = board.reduce((boardSum, row) => {
-    return boardSum + row.reduce((rowSum, cell) => rowSum + (cell !== marker ? parseInt(cell) : 0), 0);
+    return boardSum + row.reduce((rowSum, value) => rowSum + (!boardMarks[value] ? parseInt(value) : 0), 0);
   }, 0);
 
   return unmarkedSum * parseInt(lastCalledNumber);
 };
 
-const markBoard = (board: Board, num: InputType) => {
-  board.forEach((row, i) => {
-    row.forEach((cell, j) => {
-      if (cell === num) board[i][j] = marker;
-    });
-  });
+const markBoard = (boardProps: BoardProps, num: Value) => {
+  const { boardMarks } = boardProps;
+  if (boardMarks.hasOwnProperty(num)) {
+    boardMarks[num] = true;
+    boardProps.totalMarks++;
+  }
 };
 
-const isBoardWinner = (board: Board) => {
+const isBoardWinner = ({ board, boardMarks, totalMarks }: BoardProps): boolean => {
+  if (totalMarks < 5) return false;
+
   // Check Columns
   const [firstRow] = board;
   for (let i = 0; i < firstRow.length; i++) {
     const val = firstRow[i];
-    if (val === marker) {
+    if (boardMarks[val]) {
       let isWinner = true;
 
       for (let j = 1; j < board.length; j++) {
-        if (board[j][i] !== marker) {
+        if (!boardMarks[board[j][i]]) {
           isWinner = false;
           break;
         }
@@ -74,10 +83,10 @@ const isBoardWinner = (board: Board) => {
   // Check rows
   for (let i = 0; i < board.length; i++) {
     const row = board[i];
-    if (row[0] === marker) {
+    if (boardMarks[row[0]]) {
       let isWinner = true;
       for (let j = 1; j < row.length; j++) {
-        if (row[j] !== marker) {
+        if (!boardMarks[row[j]]) {
           isWinner = false;
           break;
         }
@@ -91,25 +100,29 @@ const isBoardWinner = (board: Board) => {
 };
 
 function processInput(input: string[]): Input {
-  const calledNumbers: InputType[] = input[0].split(',');
-  const boards: Array<InputType[][]> = [];
-  let board: InputType[][] = [];
+  const calledNumbers: CalledNumbers = input[0].split(',');
+  const boardPropsArr: BoardProps[] = [];
+  let board: Board = [];
+  let boardMarks: BoardMarks = {};
   for (let i = 2; i < input.length; i++) {
     const line = input[i];
     if (line) {
       const gameRow = line.trim().split(/ +/);
       board.push(gameRow);
+      gameRow.forEach((value) => (boardMarks[value] = false));
     }
 
     if (!line || i + 1 === input.length) {
-      boards.push(board);
+      boardPropsArr.push({ board, boardMarks, totalMarks: 0 });
       board = [];
+      boardMarks = {};
     }
   }
-  return [calledNumbers, boards];
+
+  return [calledNumbers, boardPropsArr];
 }
 
 const processedInput = processInput(readLines('./day04-input'));
 
-run(part1, processedInput); // part1: 39902 -- 0.8405ms
-run(part2, processedInput); // part2: 26936 -- 1.7435ms
+run(part1, processedInput); // part1: 39902 -- 0.243ms
+run(part2, processedInput); // part2: 26936 -- 0.7492ms
