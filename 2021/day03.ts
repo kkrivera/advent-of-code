5b2202ef8ad95f5c056f1885b1cc8cb8d6252c6c
Kris Rivera <kkrivera@gmail.com>
2021-12-04 15:46:25 -0500

    clean up day 03 code

--- 2021/day03.ts
+++ 2021/day03.ts
@@ -5,11 +5,9 @@ type Input = Array<'0' | '1'>;
 const part1 = (input: Input) => {
   const gamma: string[] = [];
   const epsilon: string[] = [];
-
   const threshold = input.length / 2;
   for (let i = 0; i < input[0].length; i++) {
     const ones = input.reduce((acc, line) => acc + (line[i] === '1' ? 1 : 0), 0);
-
     const isGammaOne = ones > threshold;
     gamma.push(isGammaOne ? '1' : '0');
     epsilon.push(isGammaOne ? '0' : '1');
@@ -21,26 +19,18 @@ const part1 = (input: Input) => {
 const part2 = (input: Input) => {
   let o2Input: string[] = input;
   let co2Input: string[] = input;
-  let bit: number = 0;
-  while (bit < input[0].length) {
-    if (o2Input.length > 1) {
-      const o2Ones: string[] = [];
-      const o2Zeroes: string[] = [];
-      o2Input.forEach((line) => (line[bit] === '1' ? o2Ones.push(line) : o2Zeroes.push(line)));
-      o2Input = o2Ones.length >= o2Zeroes.length ? o2Ones : o2Zeroes;
-    }
-
-    if (co2Input.length > 1) {
-      const co2Ones: string[] = [];
-      const co2Zeroes: string[] = [];
-      co2Input.forEach((line) => (line[bit] === '1' ? co2Ones.push(line) : co2Zeroes.push(line)));
-      co2Input = co2Zeroes.length <= co2Ones.length ? co2Zeroes : co2Ones;
-    }
-
-    bit++;
+  for (let i = 0; i < input[0].length; i++) {
+    if (o2Input.length > 1) o2Input = part2Helper(o2Input, i);
+    if (co2Input.length > 1) co2Input = part2Helper(co2Input, i, true);
   }
-
   return parseInt(o2Input.join(''), 2) * parseInt(co2Input.join(''), 2);
+};
+
+const part2Helper = (input: string[], idx: number, invert: boolean = false) => {
+  const ones: string[] = [];
+  const zeroes: string[] = [];
+  input.forEach((line) => (line[idx] === '1' ? ones.push(line) : zeroes.push(line)));
+  return zeroes.length > ones.length ? (invert ? ones : zeroes) : invert ? zeroes : ones;
 };
 
 const fullInput = readLines('./day03-input') as Input;
