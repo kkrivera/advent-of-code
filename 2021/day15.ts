fceabe74b6513c082aca942c54a64062b2a2da26
Kris Rivera <kkrivera@gmail.com>
2021-12-15 23:09:09 -0500

    Day 15 solution

--- 2021/day15.ts
+++ 2021/day15.ts
@@ -1,4 +1,4 @@
-import { readLines } from '../run';
+import { readLines, run } from '../run';
 
 type Coordinate = [number, number];
 type Row = number[];
@@ -22,29 +22,73 @@ const part2 = (input: Input) => {
     })
   );
 
-  return getSolution(expandedInput);
+  return getComplexSutions(expandedInput);
 };
+
+const getComplexSutions = (input: Input) => {
+  const end: Coordinate = [input.length - 1, input[0].length - 1];
+  const solutions: Solutions = Array.from({ length: input.length }, () =>
+    Array.from({ length: input[0].length }, () => Number.MAX_SAFE_INTEGER)
+  );
+  const [ei, ej] = end;
+  solutions[ei][ej] = input[ei][ej];
+
+  let lastSolution = 0;
+  let solution = -1;
+
+  while (lastSolution != solution) {
+    for (let i = ei; i >= 0; i--) {
+      for (let j = ej; j >= 0; j--) {
+        const value = getCellValue([i, j], solutions);
+        const right: Coordinate = [i, j + 1];
+        const up: Coordinate = [i - 1, j];
+        const left: Coordinate = [i, j - 1];
+        const down: Coordinate = [i + 1, j];
+
+        updateSolution(up, value, solutions, input);
+        updateSolution(right, value, solutions, input);
+        updateSolution(left, value, solutions, input);
+        updateSolution(down, value, solutions, input);
+      }
+    }
+    lastSolution = solution;
+    solution = solutions[0][0] - input[0][0];
+  }
+
+  return solution;
+};
+
+function updateSolution(coordinate: Coordinate, modifier: number, solutions: Solutions, input: Input) {
+  if (isValidCell(coordinate, solutions)) {
+    const solution = getCellValue(coordinate, solutions);
+    const proposal = modifier + getCellValue(coordinate, input);
+    if (proposal < solution) {
+      const [si, sj] = coordinate;
+      solutions[si][sj] = proposal;
+    }
+  }
+}
 
 const getSolution = (input: Input) => {
   const end: Coordinate = [input.length - 1, input[0].length - 1];
   const solutions: Solutions = Array.from({ length: input.length }, () => Array.from({ length: input[0].length }));
   const [ei, ej] = end;
-  solutions[ei][ej] = getValue(end, input);
+  solutions[ei][ej] = getCellValue(end, input);
 
   for (let i = ei; i >= 0; i--) {
     for (let j = ej; j >= 0; j--) {
-      const value = getValue([i, j], input);
+      const value = getCellValue([i, j], input);
       const down: Coordinate = [i + 1, j];
       const right: Coordinate = [i, j + 1];
 
       let downSolution: Solution;
       if (isValidCell(down, input)) {
-        downSolution = value + getValue(down, solutions);
+        downSolution = value + getCellValue(down, solutions);
       }
 
       let rightSolution: Solution;
       if (isValidCell(right, input)) {
-        rightSolution = value + getValue(right, solutions);
+        rightSolution = value + getCellValue(right, solutions);
       }
 
       if (!downSolution && !!rightSolution) {
@@ -60,30 +104,18 @@ const getSolution = (input: Input) => {
   return solutions[0][0] - input[0][0];
 };
 
-const isValidCell = ([i, j]: Coordinate, input: Input) => {
+const isValidCell = <T>([i, j]: Coordinate, input: T[][]) => {
   return i >= 0 && i < input.length && j >= 0 && j < input[i].length;
 };
 
-const getValue = <T>([i, j]: Coordinate, input: T[][]): T => {
+const getCellValue = <T>([i, j]: Coordinate, input: T[][]): T => {
   return input[i][j];
 };
 
-const exampleInput = `1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581`.split('\n');
-
 const fullInput = readLines('./day15-input');
-
 const processInput = (input: string[]): Input => {
   return input.map((line) => line.split('').map((cell) => parseInt(cell)));
 };
 
-console.log(part1(processInput(fullInput)));
-console.log(part2(processInput(fullInput)));
+run(part1, processInput(fullInput)); // part1: 626 -- 0.8936ms
+run(part2, processInput(fullInput)); // part2: 2966 -- 148.1752ms
